2bfa65d40715d215d32cc2d935eff1d558854ef8
nikita <18nikita99@gmail.com>
2020-12-09 14:15:44 +0300

    Code style fix

--- core/models.py
+++ core/models.py
@@ -5,8 +5,8 @@ class ProductOwner(models.Model):
     owner_name = models.CharField(max_length=20, default='', blank=True, null=True)
     owner_id = models.IntegerField()
 
-    def __str__(self):
-        return f'{self.owner_name} - {self.owner_id}'
+    class Meta:
+        db_table = 'product_owners'
 
 
 class Product(models.Model):
@@ -14,5 +14,5 @@ class Product(models.Model):
     product_id = models.IntegerField()
     owner_id = models.IntegerField()
 
-    def __str__(self):
-        return f'{self.product_name} - {self.product_id} - {self.owner_id}'
+    class Meta:
+        db_table = 'products'
--- core/views.py
+++ core/views.py
@@ -2,8 +2,6 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
 
-
-
 @api_view(['GET'])
 def index(request):
     data = {'username': 'Stas', 'id': 12}
